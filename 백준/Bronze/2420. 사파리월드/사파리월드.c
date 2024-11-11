8e4a02a3e2fa780ab44604de2c1053599aa1a505
surcrious1 <kimes1923@gmail.com>
2024-11-11 21:37:33 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	long long a, b;
+	scanf("%lld %lld", &a, &b);
+	if (a-b>=0) {
+		printf("%lld", a - b);
+	}
+	else {
+		printf("%lld", b - a);
+	}
+	return 0;
+}
