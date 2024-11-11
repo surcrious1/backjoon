66c4ab0f2f1d0d490b89fdfc66bec11a76372c1d
surcrious1 <kimes1923@gmail.com>
2024-11-11 22:44:25 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+int main(void) {
+	int n, k, a, b;
+	scanf("%d %d", &n, &k);
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &a, &b);
+	}
+	printf("비와이");
+	return 0;
+}
