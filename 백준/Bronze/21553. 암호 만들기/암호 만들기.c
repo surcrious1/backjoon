c098d958043a4d01b79b252a3117d7aa7caaab04
surcrious1 <kimes1923@gmail.com>
2024-11-11 22:11:15 +0900

    [Bronze III] Title: 암호 만들기, Time: 0 ms, Memory: 5016 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21553. 암호 만들기/암호 만들기.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+
+int arr[1000001];
+int main(void) {
+	char a[101], b[101], k[101];
+	scanf("%s", a);
+	scanf("%s", k);
+	printf("%s", k);
+	return 0;
+}
