bc47ad8714ee9deb1a50899e7ea6ae15daa38692
surcrious1 <kimes1923@gmail.com>
2024-11-11 21:58:34 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    a = a % 10 * 100 + (a % 100) / 10 * 10 + a / 100;
+    b = b % 10 * 100 + (b % 100) / 10 * 10 + b / 100;
+    if (a > b) printf("%d", a);
+    else printf("%d", b);
+}
