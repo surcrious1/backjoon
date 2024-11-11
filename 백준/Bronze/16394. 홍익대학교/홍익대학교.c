3b2e3997b30d3e6f1654b5d4a979da9cf9a53c20
surcrious1 <kimes1923@gmail.com>
2024-11-11 22:59:33 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<math.h>
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	printf("%d", n - 1946);
+	return 0;
+}
