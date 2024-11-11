f7c2fb7c569cfaea73f8900d56e25004415c1907
surcrious1 <kimes1923@gmail.com>
2024-11-11 23:03:46 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 1116 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include<ctype.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		printf("SciComLove\n");
+	}
+	return 0;
+}
