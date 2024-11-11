c47cb4fda62ac17ca088e11535bbc5d94d49f3c4
surcrious1 <kimes1923@gmail.com>
2024-11-11 22:19:42 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<math.h>
+#include<stack>
+
+using namespace std;
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	while (n != 0) {
+		printf("%d\n", n);
+		n--;
+	}
+	return 0;
+}
