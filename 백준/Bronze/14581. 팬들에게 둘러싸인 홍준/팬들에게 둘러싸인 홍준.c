bc23f8f0f19b56c62dc132d1d44885b0634e6862
surcrious1 <kimes1923@gmail.com>
2024-11-11 22:44:13 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+int main(void) {
+	char str[21];
+	scanf("%s", str);
+	printf(":fan::fan::fan:\n");
+	printf(":fan::%s::fan:\n",str);
+	printf(":fan::fan::fan:\n");
+	return 0;
+}
