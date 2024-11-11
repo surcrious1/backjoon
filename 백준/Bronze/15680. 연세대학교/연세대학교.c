b173b563795e858f06a57486fa4fe3420764045f
surcrious1 <kimes1923@gmail.com>
2024-11-11 22:54:17 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	if (n == 0)printf("YONSEI");
+	else if (n == 1)printf("Leading the Way to the Future");
+	return 0;
+}
