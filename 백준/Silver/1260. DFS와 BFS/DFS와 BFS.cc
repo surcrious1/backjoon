ac62f20bcbdca40fad62cca1f6c439ebe1e7db8f
surcrious1 <kimes1923@gmail.com>
2024-11-17 17:18:20 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 5948 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,66 +1,53 @@
-#include <iostream>
-#include <vector>
-#include <string.h>
-#include <queue>
-#include <algorithm>
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<stdio.h>
+#include<cstring>
+#include<cmath>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<vector>
+#include<algorithm>
+
 using namespace std;
- 
-vector<int> vec[10002];
-vector<int> result_bfs;
-vector<int> result_dfs;
-bool visit[1002];
- 
-void bfs(int temp){
-    queue<int> q;
-    q.push(temp);
-    visit[temp] = true;
-    while(!q.empty()){
-        int x = q.front();
-        q.pop();
-        result_bfs.push_back(x);
- 
-        for (int i = 0; i < vec[x].size(); i++){
-            if(!visit[vec[x][i]]){ //방문하지 않은 곳만 탐색
-                q.push(vec[x][i]); 
-                visit[vec[x][i]] = true;
-            }
-        }
-    }
+int n, arr[1001][1001] = { 0, }, check1[1001] = { 0, }, check2[1001] = { 0, };
+void dfs(int now) {
+	check1[now] = 1;
+	printf("%d ", now);
+	for (int i = 1; i <= n; i++) {
+		if (arr[now][i] == 1 && check1[i] != 1) {
+			check1[i] = 1;
+			dfs(i);
+		}
+	}
 }
- 
-void dfs(int x){
-    visit[x] = true;
-    result_dfs.push_back(x);
- 
-    for (int i = 0; i < vec[x].size(); i++){
-        if(!visit[vec[x][i]]){ //방문하지 않은 곳만 탐색
-            dfs(vec[x][i]);
-        }
-    }
+void bfs(int now) {
+	queue<int> q;
+	int node;
+	q.push(now);
+	check2[now] = 1;
+	while (!q.empty()) {
+		node = q.front();
+		printf("%d ", node);
+		q.pop();
+		for (int i = 1; i <= n; i++) {
+			if (arr[node][i] == 1 && check2[i] != 1) {
+				check2[i] = 1;
+				q.push(i);
+			}
+		}
+	}
 }
- 
-int main(){
-    int n, m, v, a, b;
-    cin >> n >> m >> v;
- 
-    for (int i = 1; i <= m;i++){
-        cin >> a >> b;
-        vec[a].push_back(b); //양방향 간선처리
-        vec[b].push_back(a); //양방향 간선처리
-    }
-    for (int i = 1; i <= n;i++){
-        sort(vec[i].begin(), vec[i].end()); // 낮은 숫자부터 탐색.
-    }
-    bfs(v);
-    memset(visit, false, sizeof(visit));
-    dfs(v);
-    for (int i = 0; i < result_dfs.size() ;i++){
-        cout << result_dfs[i] << " ";
-    }
-    cout << '\n';
-    for (int i = 0; i < result_bfs.size() ;i++){
-        cout << result_bfs[i] << " ";
-    }
-    
-    return 0;
+int main(void) {
+	int m, v, a, b;
+	scanf("%d %d %d", &n, &m, &v);
+	for (int i = 0; i < m; i++) {
+		scanf("%d %d", &a, &b);
+		arr[a][b] = 1;
+		arr[b][a] = 1;
+	}
+	dfs(v);
+	printf("\n");
+	bfs(v);
+	return 0;
 }
